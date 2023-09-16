63f66c1486948af111d6c91a65bf37a369289fe5
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-09-16 18:59:05 +0200

    Feature: finish design

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import { BrowserRouter } from "react-router-dom"
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
-import reportWebVitals from './reportWebVitals';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
-
-reportWebVitals();
