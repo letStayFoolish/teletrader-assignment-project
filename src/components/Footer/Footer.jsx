0ba1f335a149e58c654b6f8acf79e45e1ea1bc2a
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-09-17 18:23:23 +0200

    Update: Footer
    - fix: remove links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,26 +12,26 @@ const Footer = () => {
           <p className='uppercase font-normal text-base mb-2 tracking-out-expand'>A step ahead of market</p>
         </div>
 
-        <div className='hidden md:flex flex-1 justify-end md:mt-0 mt-10 mb-4'>
-            {footerLinks.map((link, _index) => (
-              <div key={_index} className='flex flex-col sm:my-0 my-4 mr-16 last:mr-0'>
-                <h4 className='font-medium text-[18px] leading-[27px]'>{link.title}</h4>
+        {/*<div className='hidden md:flex flex-1 justify-end md:mt-0 mt-10 mb-4'>*/}
+        {/*    {footerLinks.map((link, _index) => (*/}
+        {/*      <div key={_index} className='flex flex-col sm:my-0 my-4 mr-16 last:mr-0'>*/}
+        {/*        <h4 className='font-medium text-[18px] leading-[27px]'>{link.title}</h4>*/}
 
-                <ul className='list-none mt-4'>
-                  {link.links.map((l, _i) => (
-                    <li
-                      key={_i}
-                      className={`font-normal text-[16px] leading-[18px] text-dimWhite hover:opacity-75 transition-all cursor-pointer ${
-                        _i !== link.links.length - 1 ? 'mb-2' : 'mb-0'
-                      }`}
-                    >
-                      <a href={l.link} target='_blank' rel='noopener noreferrer'>{l.name}</a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ))}
-        </div>
+        {/*        <ul className='list-none mt-4'>*/}
+        {/*          {link.links.map((l, _i) => (*/}
+        {/*            <li*/}
+        {/*              key={_i}*/}
+        {/*              className={`font-normal text-[16px] leading-[18px] text-dimWhite hover:opacity-75 transition-all cursor-pointer ${*/}
+        {/*                _i !== link.links.length - 1 ? 'mb-2' : 'mb-0'*/}
+        {/*              }`}*/}
+        {/*            >*/}
+        {/*              <a href={l.link} target='_blank' rel='noopener noreferrer'>{l.name}</a>*/}
+        {/*            </li>*/}
+        {/*          ))}*/}
+        {/*        </ul>*/}
+        {/*      </div>*/}
+        {/*    ))}*/}
+        {/*</div>*/}
       </div>
 
       <div className='w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3e45]'>
